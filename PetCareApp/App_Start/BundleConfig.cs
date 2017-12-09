2544fce7e2b7f13ff7a3e49164de0b0fb35759a7
AnaLefinir <analefinir@gmail.com>
2017-12-09 00:34:31 -0300

    It's alive!

--- PetCareApp/App_Start/BundleConfig.cs
+++ PetCareApp/App_Start/BundleConfig.cs
@@ -8,42 +8,42 @@ namespace PetCareApp
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            bundles.Add(new StyleBundle("~/Content/bootstrap").Include(
+                "~/Content/bootstrap.css",
+                "~/Content/vendor/font-awesome/css/font-awesome.min.css"));
+
+            bundles.Add(new StyleBundle("~/Content/templates/dark").Include(
+                "~/Content/css/font.css",
+                "~/Content/css/style.default.css"
+            ));
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/umd/popper.js",
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css"));
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                "~/Scripts/modernizr-*"));
 
-            bundles.Add(new StyleBundle("~/Content/Login").Include(
-                "~/Content/login.css"));
+            bundles.Add(new ScriptBundle("~/bundles/templates/dark").Include(
+                "~/Content/js/front.js"
+            ));
+            //Login view
+            bundles.Add(new StyleBundle("~/Content/views/login").Include(
+                "~/Content/views/login.css"));
 
-
-            bundles.Add(new StyleBundle("~/Content/AdmiIndex").Include(
-                "~/Content/vendor/bootstrap/css/bootstrap.min.css",
-                "~/Content/vendor/font-awesome/css/font-awesome.min.css",
-                "~/Content/css/font.css",
-                "~/Content/css/style.default.css",
-                "~/Content/css/custom.css"
-                ));
-
-            bundles.Add(new ScriptBundle("~/Content/jsadmi").Include(
-                "~/Content/vendor/bootstrap/js/bootstrap.min.js",
-                "~/Content/js/front.js"));
-
-            bundles.Add(new StyleBundle("~/Content/Admin").Include(
-                "~/Content/admin.css"));
+            //Home view
+            bundles.Add(new StyleBundle("~/Content/views/home").Include(
+                "~/Content/views/home.css"));
+           
         }
     }
 }
